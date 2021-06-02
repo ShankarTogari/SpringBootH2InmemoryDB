e0386ef1caa871bf474442b3eb23e6976859de22
91879 <91879@Shankar>
2021-06-02 10:45:08 +0530

    8391-statements were pulled from github.com package, class, method are created in sts local machine and commiting to github.com

--- src/main/java/org/sfw/boot/h2db/TestingClassPullOperation.java
+++ src/main/java/org/sfw/boot/h2db/TestingClassPullOperation.java
@@ -1,3 +1,13 @@
-This file has been cretated in github.com 
-this file is created for pull operation
-which means afile createed in github is pullled to sts local machine
+package org.sfw.boot.h2db;
+
+//package declaration has also been added in sts local
+public class TestingClassPullOperation {
+
+	// This file has been cretated in github.com
+	// this file is created for pull operation
+	// which means afile createed in github is pullled to sts local machine
+
+	public void main() {
+//	This method has been created in sts local machine and commited to github.com
+	}
+}
